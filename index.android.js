509afbd17ce8b2b3f3b6439b49544bb3d73900a8
Karan Thakkar <karanjthakkar@gmail.com>
2016-06-02 00:05:38 +0530

    Change name to tweetify

--- index.android.js
+++ index.android.js
@@ -11,7 +11,7 @@ import {
   View
 } from 'react-native';
 
-class ListerApp extends Component {
+class TweetifyApp extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -48,4 +48,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('ListerApp', () => ListerApp);
+AppRegistry.registerComponent('TweetifyApp', () => TweetifyApp);
